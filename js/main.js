6f6f51a30655175cc3a87d1a4d1b164100b92cc7
David Etiosa Ojo <davidetiosaojo@gmail.com>
2020-07-19 16:27:36 +0100

    Slider implemented with glider js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // Initializing feather icons
-
 feather.replace()
 
 // Nav scroll
@@ -24,3 +23,53 @@ window.onscroll = () =>{
     if(this.scrollY <= 40) aclr.style.stroke = "white";
     else  aclr.style.stroke = "black";
 };
+
+// Calling glider js
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
+        // Mobile-first defaults
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        scrollLock: false,
+        draggable: true,
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 775,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 'auto',
+                slidesToScroll: '1',
+              }
+            },
+            {
+              // screens greater than >= 960px
+              breakpoint: 960,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 9,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              // screens greater than >= 1200px
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 11,
+                slidesToScroll: 1,
+              }
+            }
+        ]
+    });
+  })
